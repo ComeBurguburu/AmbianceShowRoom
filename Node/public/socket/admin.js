008480fd67d4ca4758ac6459463b151a0b5dde2f
benjamin <benjamin.jornet@gmail.com>
2015-12-17 13:35:55 +0100

    merge Public

--- Node/public/socket/admin.js
+++
@@ -1,9 +0,0 @@
-/*global io*/
-var socket = io.connect();
-/**
- * Envoi d'un message
- */
-socket.on('disconnect', function(){
-	alert("server is offline");
-});
-socket.emit('Event','message test');
