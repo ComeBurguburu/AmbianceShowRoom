d97043c783ffc32b5f931a41fe836c91d27d6774
HuguesCharbon <charbonhugues@gmail.com>
2016-01-05 10:41:21 +0100

    DragAndDrop

--- Node/Public/js/controllers/DropZoneController.js
+++ Node/Public/js/controllers/DropZoneController.js
@@ -2,31 +2,38 @@
 angular.module('App').controller('dropzoneController', function ($scope) {
     $scope.centerAnchor = true;
     $scope.toggleCenterAnchor = function () {$scope.centerAnchor = !$scope.centerAnchor}
-    $scope.draggableObjects = [{name:'one'}, {name:'two'}, {name:'three'}];
+    $scope.draggableObjects = [{name:'one', src:'../images/0.jpg'}, {name:'two', src:'../images/1.jpg'}, {name:'three', src:'../images/2.jpg'}];
     $scope.droppedObjects1 = [];
     $scope.droppedObjects2= [];
+
+    // var img = document.createElement("img");
+    // img.src = "../images/0.jpg";
+    // var src = document.getElementById("connard");
+    // src.appendChild(img);
+
     $scope.onDropComplete1=function(data,evt){
+
         var index = $scope.droppedObjects1.indexOf(data);
         if (index == -1)
-        $scope.droppedObjects1.push(data);
+         $scope.droppedObjects1[0]=data;
     }
-    $scope.onDragSuccess1=function(data,evt){
-        console.log("133","$scope","onDragSuccess1", "", evt);
-        var index = $scope.droppedObjects1.indexOf(data);
-        if (index > -1) {
-            $scope.droppedObjects1.splice(index, 1);
-        }
-    }
-    $scope.onDragSuccess2=function(data,evt){
-        var index = $scope.droppedObjects2.indexOf(data);
-        if (index > -1) {
-            $scope.droppedObjects2.splice(index, 1);
-        }
-    }
+    // $scope.onDragSuccess1=function(data,evt){
+    //     console.log("Bonjour");
+    //     var index = $scope.droppedObjects1.indexOf(data);
+    //     if (index > -1) {
+    //         $scope.droppedObjects1.splice(index, 1);
+    //     }
+    // }
+    // $scope.onDragSuccess2=function(data,evt){
+    //     var index = $scope.droppedObjects2.indexOf(data);
+    //     if (index > -1) {
+    //         $scope.droppedObjects2.splice(index, 1);
+    //     }
+    // }
     $scope.onDropComplete2=function(data,evt){
         var index = $scope.droppedObjects2.indexOf(data);
         if (index == -1) {
-            $scope.droppedObjects2.push(data);
+            $scope.droppedObjects2[0]=data;
         }
     }
     var inArray = function(array, obj) {
