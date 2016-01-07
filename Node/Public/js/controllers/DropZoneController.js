c7ab9af3a0c42b8a8f94c19cd0f6ca99b4348913
HuguesCharbon <charbonhugues@gmail.com>
2016-01-07 10:51:25 +0100

    ScrollBar

--- Node/Public/js/controllers/DropZoneController.js
+++ Node/Public/js/controllers/DropZoneController.js
@@ -2,7 +2,7 @@ angular.module('App').controller('dropzoneController', ['$scope' ,'sockserv', fu
 
     $scope.centerAnchor = true;
     $scope.toggleCenterAnchor = function () {$scope.centerAnchor = !$scope.centerAnchor}
-    $scope.draggableObjects = [{id: 0, src:'../images/0.jpg'}, {id:1, src:'../images/1.jpg'}, {id:2, src:'../images/2.jpg'}];
+    $scope.draggableObjects = [{id: 0, src:'../images/0.jpg'}, {id:1, src:'../images/1.jpg'}, {id:2, src:'../images/2.jpg'}, {id:3, src:'../images/3.jpg'}, {id:4, src:'../images/4.jpg'}, {id:5, src:'../images/5.jpg'}, {id:6, src:'../images/6.jpg'}, {id:7, src:'../images/7.jpg'}, {id:8, src:'../images/8.jpg'}, {id:9, src:'../images/9.jpg'}];
     $scope.droppedObjects1 = [];
  
 
