41dde15577478000465ee49751d2876cfd484692
CômeB <come.burguburu@gmail.com>
2016-01-19 10:27:57 +0100

    CROS working

--- Node/app.js
+++ Node/app.js
@@ -28,6 +28,11 @@ IOController.listen(server);
 var multerMiddleware = multer({
 	"dest": "tmp/"
 });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(uploadRoute);
 app.use(listRoute);
